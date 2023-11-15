82a8972914369e18b50c63d85d1af3a0de4c7a0e
bcordova2 <bcordovaliendo@gmail.com>
2023-11-15 18:40:51 -0500

    changes

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 const main = async () => {
     try {
-        sequelize.sync();
+        await sequelize.sync();
         console.log("DataBase connected");
         app.listen(PORT);
         console.log(`Server running on port ${PORT}`);
